3e7df0b8d3b945fe2d6e3d49604e1c893279ff6f
Aliaksandr Kuzmitski <technodeath@gmail.com>
2019-11-25 23:40:54 +0300

    added generator of random info for adding contacts and groups

--- test/add_contact_test.py
+++ test/add_contact_test.py
@@ -1,16 +1,53 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone(maxlen):
+    return "+" + "".join([random.choice(string.digits) for i in range(random.randrange(maxlen))])
+
+
+def random_day():
+    return str(random.randrange(1, 31))
+
+
+def random_month():
+    return random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                           'October', 'November', 'December'])
+
+
+def random_year():
+    return str(random.randrange(2000))
+
+
+def random_email(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@"\
+           + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + ".com"
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+            photo='E:\\1332955017586.jpg', title=random_string("title", 10), company=random_string("company", 10),
+            address=random_string("address", 10), home=random_phone(10), mobile=random_phone(10),
+            work=random_phone(10), fax=random_phone(10), email=random_email(10), email2=random_email(10),
+            email3=random_email(10), homepage=random_string("home", 10), bday=random_day(), bmonth=random_month(),
+            byear=random_year(), aday=random_day(), amonth=random_month(), ayear=random_year(),
+            address2=random_string("address2", 10), phone2=random_phone(10), notes=random_string("note", 10))
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contacts_list()
-    contact = Contact(firstname='Aliaksandr', middlename='Igorevich', lastname='Kuzmitski',
-                                       nickname='technodeath', photo='E:\\1332955017586.jpg', title='QA',
-                                       company='ScienceSoft', address='No address', home='No home', mobile='No',
-                                       work='No work', fax='No fax', email='No email1', email2='No email2',
-                                       email3='No email3', homepage='No homepage', bday='1', bmonth='September',
-                                       byear='1941', aday='9', amonth='May', ayear='1945', address2='TestAddress',
-                                       phone2='Test phone2', notes='test note')
     app.contact.press_add_new_contact()
     app.contact.fill_contact_info(contact)
     assert len(old_contacts) + 1 == app.contact.count()
--- test/add_group_test.py
+++ test/add_group_test.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group_testcase(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group_testcase(app, group):
     old_groups = app.group.get_groups_list()
-    group = Group(name='test name', header='test header', footer='test footer')
     app.group.create()
     app.group.fill_and_submit_new_group(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_groups_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group_testcase(app):
-#     old_groups = app.group.get_groups_list()
-#     group = Group(name='', header='', footer='')
-#     app.group.create()
-#     app.group.fill_and_submit_new_group(group)
-#     new_groups = app.group.get_groups_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
