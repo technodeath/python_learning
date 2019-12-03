8db4e8de691e4a03b8a137a5c9c3d9f720c57e6d
Aliaksandr Kuzmitski <technodeath@gmail.com>
2019-12-04 01:45:06 +0300

    added test for ui contacts list vs db contacts list

--- test/db_matches_ui_test.py
+++ test/db_matches_ui_test.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
@@ -8,3 +9,9 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contacts_list()
+    db_list = db.get_contact_list()
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
